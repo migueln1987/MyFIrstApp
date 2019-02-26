c36dfed064ae332141de0330ad369a97a57fd7b6
davidzimmerly <davidazimmerly@gmail.com>
2019-02-26 14:29:23 -0500

    added the lifecycle methods to override!

--- app/src/main/java/com/example/myfirstapp/DavidActivity.java
+++ app/src/main/java/com/example/myfirstapp/DavidActivity.java
@@ -10,4 +10,34 @@ public class DavidActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_david);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 }
