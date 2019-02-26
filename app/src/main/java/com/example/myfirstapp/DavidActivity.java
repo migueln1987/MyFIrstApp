3ad90ae2a6ccbd7116290b25ebe08ab27f52084e
davidzimmerly <davidazimmerly@gmail.com>
2019-02-26 14:50:48 -0500

    added logs to the lifecycle methods!

--- app/src/main/java/com/example/myfirstapp/DavidActivity.java
+++ app/src/main/java/com/example/myfirstapp/DavidActivity.java
@@ -2,42 +2,54 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DavidActivity extends AppCompatActivity {
+
+    private static final String TAG = "DavidActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_david);
+        Log.d(TAG, "onCreate: ");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart: ");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume: ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d(TAG, "onPause: ");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d(TAG, "onStop: ");
     }
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
     }
 
     @Override
     protected void onRestart() {
+
         super.onRestart();
+        Log.d(TAG, "onRestart: ");
     }
 }
